ec36b5ae13c8ee01934e272b17d022d9fb6ba0fa
Harshith <harshith.kotian24@gmail.com>
2024-07-29 23:01:27 +0530

    lazylist hack? ;)

--- app/src/main/java/com/harrrshith/moowe/ui/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/harrrshith/moowe/ui/onboarding/OnBoardingScreen.kt
@@ -4,55 +4,40 @@ import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.EaseInCubic
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.harrrshith.moowe.R
 import com.harrrshith.moowe.ui.theme.MooweTheme
 import kotlinx.coroutines.delay
@@ -76,8 +61,13 @@ fun OnBoardingScreen(
         R.drawable.image_one,
         R.drawable.image_two,
         R.drawable.image_four,
-//        R.drawable.image_four,
-//        R.drawable.image_five,
+        R.drawable.image_four,
+        R.drawable.image_five,
+        R.drawable.image_one,
+        R.drawable.image_two,
+        R.drawable.image_four,
+        R.drawable.image_four,
+        R.drawable.image_five,
     )
     val gradientOne = Brush.linearGradient(
         colors = listOf(
@@ -111,84 +101,99 @@ fun OnBoardingScreen(
                 )
             }
         }
-
     }
-
-    ArcList(
-        modifier = modifier,
-        radius = 270.dp,
-    ) { index, rotationAngle ->
-        Image(
-            painter = painterResource(id = imageList[index]),
-            contentDescription = "image $index",
-            modifier = Modifier
-                .rotate(rotationAngle)
-                .clip(RoundedCornerShape(12.dp)),
-            contentScale = ContentScale.Crop
-        )
-    }
-
+    ArcImageList(imageList = imageList)
     LaunchedEffect(key1 = backgroundState) {
         delay(4000)
         backgroundState = if(backgroundState == 1) 2 else 1
     }
 }
 
-@SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
-fun ArcList(
-    modifier: Modifier = Modifier,
-    radius: Dp,
-    itemSpacing: Dp = 60.dp,
-    itemWidth: Dp = 100.dp,
-    itemHeight: Dp = 160.dp,
-    content: @Composable (Int, Float) -> Unit
-) {
-    BoxWithConstraints(modifier) {
-        Layout(
-            content = {
-                for (i in 0 until 3) {  // Assuming 5 images, adjust as needed
-                    val rotationAngle = when (i) {
-                        0 -> -20f
-                        1 -> 0f
-                        2 -> 20f
-                        else -> 0f
+fun ArcImageList(imageList: List<Int>) {
+    val itemTransformations = remember { mutableStateMapOf<Int, ItemTransformation>() }
+    ArcLazyRow(
+        modifier = Modifier.fillMaxSize(),
+        radius = 180.dp,
+        itemSpacing = 20.dp
+    ) {
+        items(imageList.size) { index ->
+            val transformation = itemTransformations[index]
+            Image(
+                painter = painterResource(id = imageList[index]),
+                contentDescription = "image $index",
+                modifier = Modifier
+                    .graphicsLayer {
+                        translationX = transformation?.x?.toFloat() ?: 0f
+                        translationY = transformation?.y?.toFloat() ?: 0f
+                        rotationZ = transformation?.rotation ?: 0f
                     }
-                    content(i, rotationAngle)
-                }
-            }
-        ) { measurables, constraints ->
-            val itemWidthPx = itemWidth.toPx().toInt()
-            val itemHeightPx = itemHeight.toPx().toInt()
-            val placeables = measurables.map { measurable ->
-                measurable.measure(
-                    constraints.copy(
-                        minWidth = itemWidthPx,
-                        maxWidth = itemWidthPx,
-                        minHeight = itemHeightPx,
-                        maxHeight = itemHeightPx
-                    )
-                )
-            }
-
-            val arcRadius = radius.toPx()
-            val totalWidth = itemWidth.toPx() * placeables.size + itemSpacing.toPx() * (placeables.size - 1)
-            val arcLength = PI.toFloat() * arcRadius
-            val scale = totalWidth / arcLength
-
-            layout(constraints.maxWidth, constraints.maxHeight) {
-                val centerX = constraints.maxWidth / 2
-                val centerY = constraints.maxHeight / 2
-                placeables.forEachIndexed { index, placeable ->
-                    val angleRad = (PI.toFloat() / 2) - (index - (placeables.size - 1) / 2f) * scale
-                    val x = centerX + (arcRadius * cos(angleRad)).toInt() - placeable.width / 2
-                    val y = (centerY - (arcRadius * sin(angleRad)).toInt() + placeable.height / 1.1).toInt()
-                    placeable.place(x, y * 2)
-                }
-            }
+                    .padding(horizontal = 8.dp)
+                    .height(120.dp)
+                    .aspectRatio(.8f)
+                    .clip(RoundedCornerShape(12.dp)),
+                contentScale = ContentScale.Crop
+            )
         }
     }
 }
+
+@SuppressLint("UnusedBoxWithConstraintsScope")
+@Composable
+fun ArcLazyRow(
+    modifier: Modifier = Modifier,
+    radius: Dp,
+    itemSpacing: Dp = 20.dp,
+    content: LazyListScope.() -> Unit
+) {
+    val lazyListState = rememberLazyListState()
+    val itemTransformations = remember { mutableStateMapOf<Int, ItemTransformation>() }
+
+    Layout(
+        content = {
+            LazyRow(
+                state = lazyListState,
+                content = content,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            )
+        },
+        modifier = modifier
+    ) { measurables, constraints ->
+        val rowConstraints = constraints.copy(minWidth = 0, minHeight = 0)
+        val rowPlaceable = measurables.first().measure(rowConstraints)
+        val arcRadius = radius.toPx()
+        val itemCount = lazyListState.layoutInfo.totalItemsCount
+        val visibleItemsInfo = lazyListState.layoutInfo.visibleItemsInfo
+        val itemWidth = visibleItemsInfo.firstOrNull()?.size ?: 0
+        val totalWidth = itemWidth * itemCount + itemSpacing.toPx() * (itemCount - 1)
+        val arcLength = PI.toFloat() * arcRadius
+        val scale = totalWidth / arcLength
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            val centerX = constraints.maxWidth / 2
+            val centerY = constraints.maxHeight / 2
+
+            rowPlaceable.placeRelative(0, centerY)
+
+            visibleItemsInfo.forEach { itemInfo ->
+                val index = itemInfo.index
+                val angleRad = (PI.toFloat() / 2) + (index - (itemCount - 1) / 2f) * scale
+                val x = centerX + (arcRadius * cos(angleRad)).toInt() - itemWidth / 2
+                val y = centerY - (arcRadius * sin(angleRad)).toInt() - itemInfo.size / 2
+                val rotationAngle = 180f - (angleRad * 180f / PI.toFloat())
+                itemTransformations[index] = ItemTransformation(x, y, rotationAngle)
+            }
+            itemTransformations.map { it -> Log.e("Response", "${it.value}")}
+
+        }
+    }
+}
+
+data class ItemTransformation(
+    val x: Int,
+    val y: Int,
+    val rotation: Float
+)
 
 
 @Preview
