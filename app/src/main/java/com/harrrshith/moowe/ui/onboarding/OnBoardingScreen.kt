2e8ef268d6619a6748fe79b062992bcb05f42206
Harshith <harshith.kotian24@gmail.com>
2024-07-30 23:16:14 +0530

    animatedStateOf

--- app/src/main/java/com/harrrshith/moowe/ui/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/harrrshith/moowe/ui/onboarding/OnBoardingScreen.kt
@@ -1,10 +1,19 @@
 package com.harrrshith.moowe.ui.onboarding
 
 import android.annotation.SuppressLint
+import android.graphics.Color.alpha
 import android.util.Log
-import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.EaseInCubic
+import androidx.compose.animation.core.InfiniteTransition
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.rememberTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -29,6 +38,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -44,6 +54,7 @@ import kotlinx.coroutines.delay
 import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
+import kotlin.random.Random
 
 @Composable
 fun OnBoardingRoute() {
@@ -69,39 +80,79 @@ fun OnBoardingScreen(
         R.drawable.image_four,
         R.drawable.image_five,
     )
-    val gradientOne = Brush.linearGradient(
-        colors = listOf(
-            Color.Black,
-            MaterialTheme.colorScheme.primary,
-            MaterialTheme.colorScheme.tertiary,
-            MaterialTheme.colorScheme.onPrimaryContainer
-        )
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite animation")
+    val animatedFloatAlpha by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 5000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "alpha"
     )
 
-    val gradientTwo = Brush.linearGradient(
-        colors = listOf(
-            MaterialTheme.colorScheme.primary,
-            MaterialTheme.colorScheme.onPrimaryContainer,
-            Color.Black,
-            MaterialTheme.colorScheme.tertiary,
-        )
+    val animatedFloatAlphaReverse by infiniteTransition.animateFloat(
+        initialValue = 1f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 5000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "alpha"
     )
-    Crossfade(targetState = backgroundState, label = "", animationSpec = tween(500, easing = EaseInCubic)) {
-        when(it) {
-            1 -> {
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .background(gradientOne)
-                )
-            }
-            2 -> {
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .background(gradientTwo)
-                )
-            }
-        }
+    val colorsOne = listOf(
+        Color.Cyan,
+        MaterialTheme.colorScheme.primary,
+        MaterialTheme.colorScheme.tertiary,
+        MaterialTheme.colorScheme.onPrimaryContainer
+    )
+    val colorsTwo = listOf(
+        MaterialTheme.colorScheme.primary,
+        MaterialTheme.colorScheme.onPrimaryContainer,
+        Color.Black,
+        MaterialTheme.colorScheme.tertiary,
+    )
+    val animatedColorOne by infiniteTransition.animateColor(
+        initialValue = colorsOne[getRandom],
+        targetValue = colorsOne[getRandom],
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 3000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "animatedColorOne"
+    )
+
+    val animatedColorTwo by infiniteTransition.animateColor(
+        initialValue = colorsTwo[getRandom],
+        targetValue = colorsTwo[getRandom],
+        animationSpec = infiniteRepeatable(
+            animation = tween(durationMillis = 4000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
+        label = "animatedColorTwo"
+    )
+
+    Canvas(modifier = modifier.fillMaxSize()) {
+        val gradient1 = Brush.linearGradient(
+            colors = listOf(animatedColorOne, animatedColorTwo),
+            start = Offset(0f, 0f),
+            end = Offset(size.width, size.height)
+        )
+        val gradient2 = Brush.linearGradient(
+            colors = listOf(animatedColorTwo, animatedColorOne),
+            start = Offset(size.width, 0f),
+            end = Offset(0f, size.height)
+        )
+        drawRect(brush = gradient1, alpha = animatedFloatAlpha)
+        drawRect(brush = gradient2, alpha = animatedFloatAlphaReverse)
     }
+
+//    Box(modifier = Modifier
+//        .fillMaxSize()
+//        .background(
+//            color = Color.Cyan.copy(alpha = animatedFloatAlpha)
+//        )
+//    )
     ArcImageList(imageList = imageList)
     LaunchedEffect(key1 = backgroundState) {
         delay(4000)
@@ -137,6 +188,9 @@ fun ArcImageList(imageList: List<Int>) {
         }
     }
 }
+
+private val getRandom: Int
+    get() = Random.nextInt(0, 3)
 
 @SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
@@ -183,8 +237,6 @@ fun ArcLazyRow(
                 val rotationAngle = 180f - (angleRad * 180f / PI.toFloat())
                 itemTransformations[index] = ItemTransformation(x, y, rotationAngle)
             }
-            itemTransformations.map { it -> Log.e("Response", "${it.value}")}
-
         }
     }
 }
